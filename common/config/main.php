562621ac1af4647d6a2c21ae46cd7fb52310db84
AndersonKuai <158427081@qq.com>
2018-01-08 11:56:34 +0800

    线上数据库配置

--- common/config/main.php
+++ common/config/main.php
@@ -5,11 +5,18 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+//        'db' => [
+//            'class' => 'yii\db\Connection',
+//            'dsn' => 'mysql:host=192.168.1.19;dbname=management',
+//            'username' => 'ylmg',
+//            'password' => 'ylmg@1qazxsw2',
+//            'charset' => 'utf8',
+//        ],
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.1.19;dbname=management',
-            'username' => 'ylmg',
-            'password' => 'ylmg@1qazxsw2',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=management',
+            'username' => 'root',
+            'password' => 'WjCn0hAe8BDddB08',
             'charset' => 'utf8',
         ],
         'wnmall' => [
