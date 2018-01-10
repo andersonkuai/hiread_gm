c1de28374baa70cb31268046df47a4a70d09fa28
AndersonKuai <158427081@qq.com>
2018-01-10 16:55:03 +0800

    去掉公共配置文件的忽略

---
+++ common/config/main-local.php
@@ -0,0 +1,38 @@
+<?php
+return  DIRECTORY_SEPARATOR=='\\' ? [
+    'components' => [
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=192.168.1.19;dbname=management',
+            'username' => 'ylmg',
+            'password' => 'ylmg@1qazxsw2',
+            'charset' => 'utf8',
+        ],
+        'wnmall' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=192.168.1.19;dbname=wnmall',
+            'username' => 'ylmg',
+            'password' => 'ylmg@1qazxsw2',
+            'charset' => 'utf8',
+        ],
+    ],
+]:[
+    'components' => [
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
