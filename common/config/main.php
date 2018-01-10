e12c21700e5db8a1e3b5a04afcb7849929b80b0a
AndersonKuai <158427081@qq.com>
2018-01-10 17:26:56 +0800

    1

--- common/config/main.php
+++ common/config/main.php
@@ -19,16 +19,8 @@ return [
             'password' => 'WjCn0hAe8BDddB08',
             'charset' => 'utf8',
         ],
-        'wnmall' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.1.19;dbname=wnmall',
-            'username' => 'ylmg',
-            'password' => 'ylmg@1qazxsw2',
-            'charset' => 'utf8',
-        ],
-        'ylmgSupport' => [
-            'class' => 'common\classes\ylmg\YlmgSupportDb',
-        ],
+
+
     ],
     'language' => 'zh-CN',
 ];
