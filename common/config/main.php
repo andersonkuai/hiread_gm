5eab6ce079929aa02a9fce11f3c33f21c1f77bfb
AndersonKuai <158427081@qq.com>
2018-01-11 09:33:20 +0800

    12

--- common/config/main.php
+++ common/config/main.php
@@ -12,13 +12,13 @@ return [
 //            'password' => 'ylmg@1qazxsw2',
 //            'charset' => 'utf8',
 //        ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=management',
-            'username' => 'root',
-            'password' => 'WjCn0hAe8BDddB08',
-            'charset' => 'utf8',
-        ],
+//        'db' => [
+//            'class' => 'yii\db\Connection',
+//            'dsn' => 'mysql:host=127.0.0.1;dbname=management',
+//            'username' => 'root',
+//            'password' => 'WjCn0hAe8BDddB08',
+//            'charset' => 'utf8',
+//        ],
 
 
     ],
