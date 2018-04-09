4ec35e477bf96d313ffe492ae4e49e1e372f14d6
andersonKuai <158427081@qq.com>
2018-04-09 15:42:28 +0800

    1

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,8 @@
 <?php
+$static_dir = dirname(dirname(dirname(dirname(__FILE__)))).'/static';
 return [
     //文件域名
-    'static_hiread' => 'http://static.hiread.cn/',
+    'static_hiread' => 'https://cdn1.hiread.cn/',
     //文件上传地址
-    'course_book_img' => 'D:/HTTP/upload_tmp_dir',
+    'testDir' => $static_dir,
 ];
