adf79b9b6b4cb808d91c91790c06d772414fab8b
andersonKuai <158427081@qq.com>
2018-04-09 15:49:08 +0800

    1

--- common/config/params.php
+++ common/config/params.php
@@ -1,8 +1,81 @@
 <?php
-$static_dir = dirname(dirname(dirname(dirname(__FILE__)))).'/static';
+$static_dir = dirname(dirname(dirname(dirname(__FILE__)))).'/static/';
 return [
     //文件域名
     'static_hiread' => 'https://cdn1.hiread.cn/',
-    //文件上传地址
-    'testDir' => $static_dir,
+
+    //课程封面地址
+    'cover_img' => $static_dir.'/course/book/',
+    'view_CoverImg' => 'course/book/',//查看目录
+    //课程简介图片地址
+    'detail_img' => $static_dir.'/course/book/',
+    'view_DetailImg' => 'course/book/',//查看目录
+    //作者头像目录
+    'author_img' => $static_dir.'/course/book/',
+    'view_Author' => 'course/book/',//查看目录
+    //课程包地址
+    'package' => $static_dir.'/course/book/',
+    //每日朗读音频
+    'read_audio' => $static_dir.'/course/book/',
+    'view_read_audio' => 'course/book/',
+    /**
+     * 泛读
+     */
+    //泛读封面地址
+    'extensive_cover_img' => $static_dir.'/course/question/image/extensive/',
+    'view_extensive_cover_img' => 'course/question/image/extensive/',//查看目录
+    //泛读视频地址
+    'extensive_video' => $static_dir.'/course/question/video/extensive/',
+    'view_extensive_video' => 'course/question/video/extensive/',//查看目录
+    //泛读题目图片
+    'extensive_topic_img' => $static_dir.'/course/question/image/',
+    'view_extensive_topic_img' => 'course/question/image/',//查看目录
+    //泛读题目音频
+    'extensive_topic_audio' => $static_dir.'/course/question/audio/',
+    'view_extensive_topic_audio' => 'course/question/audio/',//查看目录
+    //泛读题目描述音频文件
+    'extensive_question_des_audio' => $static_dir.'/course/question/audio/',
+    'view_extensive_question_des_audio' => 'course/question/audio/',//查看目录
+    //泛读题目解析音频
+    'extensive_question_des_aaudio' => $static_dir.'/course/answer/audio/',
+    'view_extensive_question_des_aaudio' => 'course/answer/audio/',//查看目录
+    //泛读题目解析视频
+    'extensive_question_des_avideo' => $static_dir.'/course/answer/video/',
+    'view_extensive_question_des_avideo' => 'course/answer/video/',//查看目录
+    //泛读答案图片
+    'extensive_answer_image' => $static_dir.'/course/answer/image/',
+    'view_extensive_answer_image' => 'course/answer/image/',//查看目录
+    //泛读答案配对图片
+    'extensive_answer_Pair1Img' => $static_dir.'/course/answer/image/',
+    'view_extensive_answer_Pair1Img' => 'course/answer/image/',//查看目录
+    //泛读答案配对音频
+    'extensive_answer_Pair1Audio' => $static_dir.'/course/answer/audio/',
+    'view_extensive_answer_Pair1Audio' => 'course/answer/audio/',//查看目录
+    /**
+     * 精读
+     */
+    //题目图片
+    'unit_topic_img' => $static_dir.'/course/question/image/',
+    'view_unit_topic_img' => 'course/question/image/',//查看目录
+    //题目音频
+    'unit_topic_audio' => $static_dir.'/course/question/audio/',
+    'view_unit_topic_audio' => 'course/question/audio/',//查看目录
+    //题目描述音频
+    'unit_question_des_audio' => $static_dir.'/course/question/audio/',
+    'view_unit_question_des_audio' => 'course/question/audio/',//查看目录
+    //题目视频地址
+    'unit_topic_video' => $static_dir.'/course/question/video/',
+    'view_unit_topic_video' => 'course/question/video/',//查看目录
+    //视频封面
+    'unit_topic_poster' => $static_dir.'/course/question/image/',
+    'view_unit_topic_poster' => 'course/question/image/',//查看目录
+    //例句音频
+    'unit_SampleAudio' => $static_dir.'/course/question/audio/',
+    'view_unit_SampleAudio' => 'course/question/audio/',//查看目录
+    //题目解析音频
+    'unit_question_des_aaudio' => $static_dir.'/course/answer/audio/',
+    'view_unit_question_des_aaudio' => 'course/answer/audio/',//查看目录
+    //题目解析视频
+    'unit_question_des_avideo' => $static_dir.'/course/answer/video/',
+    'view_unit_question_des_avideo' => 'course/answer/video/',//查看目录
 ];
