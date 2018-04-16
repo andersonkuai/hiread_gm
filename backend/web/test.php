76a09caf7a4074e375ac3d90e5de9a6dac1c43ec
andersonKuai <158427081@qq.com>
2018-04-16 17:19:49 +0800

    1

--- backend/web/test.php
+++ backend/web/test.php
@@ -1,12 +1,12 @@
 <?php
 
-$redis = new Redis();
-$redis->connect('127.0.0.1',6379);
-$redis->setOption(Redis::OPT_SERIALIZER,Redis::SERIALIZER_PHP);
-$redis->set('kuai',5);
-$test = $redis->get('kuai');
-var_dump($test);
-exit;
+//$redis = new Redis();
+//$redis->connect('127.0.0.1',6379);
+//$redis->setOption(Redis::OPT_SERIALIZER,Redis::SERIALIZER_PHP);
+//$redis->set('kuai',5);
+//$test = $redis->get('kuai');
+//var_dump($test);
+//exit;
 
 
 phpinfo();
