967bdd1dc39b6a26fdc3bfcd159d044245b904dc
andersonKuai <158427081@qq.com>
2018-08-14 13:53:58 +0800

    1

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,8 +1,8 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 // defined('YII_ENV') or define('YII_ENV', '1');//本地
-// defined('YII_ENV') or define('YII_ENV', '2');//测试服
-defined('YII_ENV') or define('YII_ENV', '3');//正式服
+defined('YII_ENV') or define('YII_ENV', '2');//测试服
+// defined('YII_ENV') or define('YII_ENV', '3');//正式服
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
